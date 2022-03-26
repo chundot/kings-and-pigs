d7bf44527839e512449ec46c524238f3edb9f3cd
chundot <qq783114458@gmail.com>
2022-03-26 21:16:04 +0800

    change: move PlayerController to Common namespace.

---
+++ Scripts/Common/PlayerController.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+using kingsandpigs.Scripts.Common;
+
+namespace kingsandpigs.Scripts.Common
+{
+    public class PlayerController<TBody> : Node2D where TBody : BaseBody
+    {
+        protected TBody Body;
+        [Export] public bool Entering;
+        public bool CanEnterDoor;
+        public float JumpAction = -.1f;
+        public override void _Ready()
+        {
+            Body = GetChild<TBody>(0);
+        }
+        public override void _PhysicsProcess(float delta)
+        {
+            if (Input.IsActionJustPressed("restart")) GetTree().ReloadCurrentScene();
+        }
+
+    }
+}
--- Scripts/KingController.cs
+++ Scripts/KingController.cs
@@ -1,5 +1,6 @@
 using System;
 using Godot;
+using kingsandpigs.Scripts.Common;
 
 public class KingController : PlayerController<King>
 {
